d4db2825e92cc19b775cbab72b6886a6319d483f
adekunlelotus <Adekunle@lotusbetaanalytics.com>
2017-06-23 12:55:57 +0100

    add my simple calculator

---
+++ script.js
@@ -0,0 +1,23 @@
+function c(val) 
+{
+  document.getElementById("display").value = val;
+
+  // body...
+}
+function math(val)
+{
+  document.getElementById("display").value += val;
+}
+
+function e()
+{
+  try
+{
+  c(eval(document.getElementById("display").value))
+}
+catch(e)
+{
+  c('Error')
+}
+
+}
