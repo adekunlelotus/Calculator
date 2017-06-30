12fe7961dab79f842df4d311b18c7822d0b87ff3
adekunlelotus <Adekunle@lotusbetaanalytics.com>
2017-06-30 10:05:15 +0100

    Converted calculator application of last week to use AngularJS

--- script.js
+++ script.js
@@ -1,23 +1,67 @@
-function c(val) 
-{
-  document.getElementById("display").value = val;
+var app = angular.module('calculatorApp', []);
 
-  // body...
-}
-function math(val)
-{
-  document.getElementById("display").value += val;
+app.controller('calculatorCtrl', con);
+
+
+function con($scope){
+    $scope.display = "";
+
+    
+    $scope.math = function(val){
+      $scope.display += val;
+    };
+
+    $scope.c = function(val){
+      $scope.display = val;
+    };
+
+    $scope.e = function(){
+        $scope.display= eval($scope.display);
+    };
 }
 
-function e()
-{
-  try
-{
-  c(eval(document.getElementById("display").value))
-}
-catch(e)
-{
-  c('Error')
-}
 
-}
+
+
+/*var myApp = angular.module('myApp', []);
+
+ myApp.controller('calculatorCtrl',function ($scope) {  
+
+
+$scope.operators = ['+', '-', '*', '/'];
+$scope.selectedOperator = $scope.operators[0];
+
+
+
+ $scope.updateOutput = function (btn) {
+    if ($scope.output == "0" || $scope.newNumber) {
+        $scope.output = btn;
+        $scope.newNumber = false;
+    } else {
+        $scope.output += String(btn);
+    }
+    $scope.pendingValue = toNumber($scope.output);
+};
+
+$scope.calculate = function() {
+    var A = parseInt($scope.firstNumber);
+    var B = parseInt($scope.secondNumber);
+    var C = 0;
+
+    switch ($scope.selectedOperator) {
+        case '+':
+            C = A + B;
+            break;
+        case '-':
+            C = A - B;
+            break;
+        case '*':
+            C = A * B;
+            break;
+        case '/':
+            C = A / B;
+            break;                
+    }
+
+    $scope.result = C;
+}; });*/
